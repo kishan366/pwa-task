aee63eafcff3f05d3d13802f48f0f45bd3bfcd79
kishan patel <kp804350@gmail.com>
2026-01-01 17:05:33 +0530

    last comm

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,5 @@
 import "./globals.css";
-import ClientProviders from "../components/ClientProviders";
+import ClientProviders from "../app/components/ClientProviders";
 
 export const metadata = {
   title: "PWA Task App",
